c01eb98159335b4f3b42a5c01a005bb684a05e7b
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-03-19 11:52:17 +0100

    basic setup

---
+++ android-launcher/app/src/main/java/com/example/androidlauncher/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.androidlauncher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.content.ComponentName
+import android.content.Context
+import android.graphics.drawable.Drawable
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
