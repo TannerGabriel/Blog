72bdc63a65fb83c011d9b504d2e31f78f8ffe123
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-05-09 19:30:19 +0200

    Added image taking functionality

--- CameraX/app/src/main/java/com/example/camerax/MainActivity.kt
+++ CameraX/app/src/main/java/com/example/camerax/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.example.camerax
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.TextureView
 import androidx.camera.core.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 private const val REQUEST_CODE_PERMISSIONS = 10
 
-// This is an array of all the permission specified in the manifest
-private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+val permissions = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
 
 class MainActivity : AppCompatActivity() {
 
+    val filename = "test.png"
+    val sd = Environment.getExternalStorageDirectory()
+    val dest = File(sd, filename)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +30,13 @@ class MainActivity : AppCompatActivity() {
 
         val previewConfig = PreviewConfig.Builder().build()
         val preview = Preview(previewConfig)
+
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .setTargetRotation(windowManager.defaultDisplay.rotation)
+            .build()
+
+        val imageCapture = ImageCapture(imageCaptureConfig)
+
 
         val textureView: TextureView = findViewById(R.id.view_finder)
 
@@ -30,7 +46,41 @@ class MainActivity : AppCompatActivity() {
         }
 
         // The use case is bound to an Android Lifecycle with the following code.
-        CameraX.bindToLifecycle(this as LifecycleOwner, preview)
+        CameraX.bindToLifecycle(this as LifecycleOwner, imageCapture, preview)
+
+        fab_camera.setOnClickListener {
+            imageCapture.takePicture(dest,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(error: ImageCapture.UseCaseError,
+                                         message: String, exc: Throwable?) {
+                        Log.e("Image", error.toString())
+                        println("Error: $error")
+                    }
+                    override fun onImageSaved(file: File) {
+                        Log.v("Image", "Successfully saved image")
+                        println("Successfully saved image")
+                    }
+                })
+        }
+    }
+
+    private fun hasNoPermissions(): Boolean{
+        return ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+            Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+    }
+
+    fun requestPermission(){
+        ActivityCompat.requestPermissions(this, permissions,0)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (hasNoPermissions()) {
+            requestPermission()
+        }
     }
 
 }
