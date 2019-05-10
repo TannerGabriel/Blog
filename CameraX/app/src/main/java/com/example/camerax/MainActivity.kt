9f2e206f608bdf9a22d4a037755454ae6372747e
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-05-10 08:57:15 +0200

    Updated documentation

--- CameraX/app/src/main/java/com/example/camerax/MainActivity.kt
+++ CameraX/app/src/main/java/com/example/camerax/MainActivity.kt
@@ -16,15 +16,13 @@ import androidx.lifecycle.LifecycleOwner
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
-private const val REQUEST_CODE_PERMISSIONS = 10
-
 val permissions = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
 
 class MainActivity : AppCompatActivity() {
 
-    val filename = "test.png"
-    val sd = Environment.getExternalStorageDirectory()
-    val dest = File(sd, filename)
+    private val filename = "test.png"
+    private val sd = Environment.getExternalStorageDirectory()
+    private val dest = File(sd, filename)
     private var lensFacing = CameraX.LensFacing.BACK
     private var imageCapture: ImageCapture? = null
 
@@ -34,27 +32,28 @@ class MainActivity : AppCompatActivity() {
 
         bindCamera()
 
+        // Takes an images and saves it in the local storage
         fab_camera.setOnClickListener {
             imageCapture?.takePicture(dest,
                 object : ImageCapture.OnImageSavedListener {
                     override fun onError(error: ImageCapture.UseCaseError,
                                          message: String, exc: Throwable?) {
                         Log.e("Image", error.toString())
-                        println("Error: $error")
                     }
                     override fun onImageSaved(file: File) {
                         Log.v("Image", "Successfully saved image")
-                        println("Successfully saved image")
                     }
                 })
         }
 
+        // Changes the flash mode when the button is clicked
         fab_flash.setOnClickListener {
             val flashMode = imageCapture?.flashMode
             if(flashMode == FlashMode.ON) imageCapture?.flashMode = FlashMode.OFF
             else imageCapture?.flashMode = FlashMode.ON
         }
 
+        // Changes the lens direction if the button is clicked
         fab_switch_camera.setOnClickListener {
             lensFacing = if (CameraX.LensFacing.FRONT == lensFacing) {
                 CameraX.LensFacing.BACK
@@ -65,6 +64,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Check if the app has all permissions
+     */
     private fun hasNoPermissions(): Boolean{
         return ContextCompat.checkSelfPermission(this,
             Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
@@ -72,40 +74,52 @@ class MainActivity : AppCompatActivity() {
             Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
     }
 
+    /**
+     * Request all permissions
+     */
     fun requestPermission(){
         ActivityCompat.requestPermissions(this, permissions,0)
     }
 
+    /**
+     * Bind the Camera to the lifecycle
+     */
     private fun bindCamera(){
         CameraX.unbindAll()
 
+        // Preview config for the camera
         val previewConfig = PreviewConfig.Builder()
             .setLensFacing(lensFacing)
+            .build()
 
-        val preview = Preview(previewConfig.build())
+        val preview = Preview(previewConfig)
 
-        var imageCaptureConfig = ImageCaptureConfig.Builder()
+        // Image capture config which controls the Flash and Lens
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
             .setTargetRotation(windowManager.defaultDisplay.rotation)
             .setLensFacing(lensFacing)
             .setFlashMode(FlashMode.ON)
+            .build()
 
-        imageCapture = ImageCapture(imageCaptureConfig.build())
+        imageCapture = ImageCapture(imageCaptureConfig)
 
+        // The view that displays the preview
         val textureView: TextureView = findViewById(R.id.view_finder)
 
-        // The output data-handling is configured in a listener.
+        // Handles the output data of the camera
         preview.setOnPreviewOutputUpdateListener { previewOutput ->
+            // Displays the camera image in our preview view
             textureView.surfaceTexture = previewOutput.surfaceTexture
         }
 
-
-        // The use case is bound to an Android Lifecycle with the following code.
+        // Bind the camera to the lifecycle
         CameraX.bindToLifecycle(this as LifecycleOwner, imageCapture, preview)
     }
 
     override fun onStart() {
         super.onStart()
 
+        // Check and request permissions
         if (hasNoPermissions()) {
             requestPermission()
         }
