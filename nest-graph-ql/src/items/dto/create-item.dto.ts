aa251e97fc1381807a9dbec58037faac9b532f2e
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-05 11:16:01 +0200

    Added validation pipe

--- nest-graph-ql/src/items/dto/create-item.dto.ts
+++ nest-graph-ql/src/items/dto/create-item.dto.ts
@@ -1,13 +1,19 @@
 import { ObjectType, Field, Int, ID } from 'type-graphql';
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 
 @ObjectType()
 export class ItemType {
     @Field(() => ID)
+    @IsString()
     readonly id: string;
     @Field()
+    @IsString()
+    @IsNotEmpty()
     readonly title: string;
     @Field(() => Int)
+    @IsNumber()
     readonly price: number;
     @Field()
+    @IsString()
     readonly description: string;
 }
