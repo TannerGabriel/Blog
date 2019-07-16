30481016272b12d881be7810e150d510e1b59b8a
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-16 20:45:37 +0200

    Deleted unused files

--- NestVueChat/src/app.module.ts
+++ NestVueChat/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, AppGateway],
+  controllers: [],
+  providers: [AppGateway],
 })
 export class AppModule {}
