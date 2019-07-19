b3de01362b555c8f14c5965b24de13ba04198c95
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-19 11:18:35 +0200

    Installed dependencies and created files

--- nest-auth/src/app.module.ts
+++ nest-auth/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
