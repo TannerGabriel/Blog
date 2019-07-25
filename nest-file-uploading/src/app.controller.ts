5996158d227555394df30a8f8a9b55bb054642a6
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-25 17:12:39 +0200

    Uploading multiple files

--- nest-file-uploading/src/app.controller.ts
+++ nest-file-uploading/src/app.controller.ts
@@ -1,8 +1,22 @@
 import { Controller, Get, Post, UseInterceptors, UploadedFile, UploadedFiles, Res, Param } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
+
+const imageFileFilter = (req, file, callback) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return callback(new Error('Only image files are allowed!'), false);
+  }
+  callback(null, true);
+};
+
+const editFileName = (req, file, cb) => {
+  const name = file.originalname.split('.')[0];
+  const fileExtName = extname(file.originalname);
+  const randomName = Array(4).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+  cb(null, `${name}-${randomName}${fileExtName}`);
+};
 
 @Controller()
 export class AppController {
@@ -16,20 +30,36 @@ export class AppController {
   @Post()
   @UseInterceptors(FileInterceptor('image', {
     storage: diskStorage({
-      destination: './files'
-      , filename: (req, file, cb) => {
-        const name = file.originalname.split('.')[0];
-        const fileExtName = extname(file.originalname);
-        const randomName = Array(4).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
-        cb(null, `${name}-${randomName}${fileExtName}`);
-      },
+      destination: './files',
+      filename: editFileName,
     }),
+    fileFilter: imageFileFilter,
   }))
   async uploadedFile(@UploadedFile() file) {
     const response = {
       originalname: file.originalname,
       filename: file.filename,
     };
+    return response;
+  }
+
+  @Post('multiple')
+  @UseInterceptors(FilesInterceptor('image', 20, {
+    storage: diskStorage({
+      destination: './files',
+      filename: editFileName,
+    }),
+    fileFilter: imageFileFilter,
+  }))
+  async uploadMultipleFiles(@UploadedFiles() files) {
+    const response = [];
+    files.forEach(file => {
+      const fileReponse = {
+        originalname: file.originalname,
+        filename: file.filename,
+      };
+      response.push(fileReponse);
+    });
     return response;
   }
 
