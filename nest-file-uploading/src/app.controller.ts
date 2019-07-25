8614b1f0ce1e8579c46e774a889e8c97d290a44f
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-25 18:02:45 +0200

    Put utils in seperate folder + deleted unused files

--- nest-file-uploading/src/app.controller.ts
+++ nest-file-uploading/src/app.controller.ts
@@ -2,31 +2,10 @@ import { Controller, Get, Post, UseInterceptors, UploadedFile, UploadedFiles, Re
 import { AppService } from './app.service';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { extname } from 'path';
-
-const imageFileFilter = (req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-    return callback(new Error('Only image files are allowed!'), false);
-  }
-  callback(null, true);
-};
-
-const editFileName = (req, file, cb) => {
-  const name = file.originalname.split('.')[0];
-  const fileExtName = extname(file.originalname);
-  const randomName = Array(4).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
-  cb(null, `${name}-${randomName}${fileExtName}`);
-};
+import { editFileName, imageFileFilter } from './utils/file-upload.utils';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
   @Post()
   @UseInterceptors(FileInterceptor('image', {
     storage: diskStorage({
