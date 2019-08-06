6015e90ceb04f35f4f778cda6dc89f1afe636dc8
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-08-06 07:27:22 +0200

    Updated and cleaned code

--- prisma-crud/src/index.ts
+++ prisma-crud/src/index.ts
@@ -3,39 +3,42 @@ import { prisma } from './generated/prisma-client'
 import { Context } from './utils'
 
 const resolvers = {
-  Query: {
-    feed(parent, args, context: Context) {
-      return context.prisma.posts({ where: { published: true } })
-    },
-    drafts(parent, args, context: Context) {
-      return context.prisma.posts({ where: { published: false } })
-    },
-    post(parent, { id }, context: Context) {
-      return context.prisma.post({ id })
-    },
-  },
-  Mutation: {
-    createUser(parent, { email, password }, context: Context){
-      return context.prisma.createUser({ email, password })
-    },
-    createDraft(parent, { title, content }, context: Context) {
-      return context.prisma.createPost({ title, content })
-    },
-    deletePost(parent, { id }, context: Context) {
-      return context.prisma.deletePost({ id })
-    },
-    publish(parent, { id }, context: Context) {
-      return context.prisma.updatePost({
-        where: { id },
-        data: { published: true },
-      })
-    },
-  },
+	Query: {
+		feed(parent, args, context: Context) {
+			return context.prisma.posts({ where: { published: true } })
+		},
+		drafts(parent, args, context: Context) {
+			return context.prisma.posts({ where: { published: false } })
+		},
+		post(parent, { id }, context: Context) {
+			return context.prisma.post({ id })
+		},
+		users(parent, args, context: Context) {
+			return context.prisma.users()
+		},
+	},
+	Mutation: {
+		createUser(parent, { email, password }, context: Context) {
+			return context.prisma.createUser({ email, password })
+		},
+		createDraft(parent, { title, content }, context: Context) {
+			return context.prisma.createPost({ title, content })
+		},
+		deletePost(parent, { id }, context: Context) {
+			return context.prisma.deletePost({ id })
+		},
+		publish(parent, { id }, context: Context) {
+			return context.prisma.updatePost({
+				where: { id },
+				data: { published: true },
+			})
+		},
+	},
 }
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers,
-  context: { prisma },
+	typeDefs: './src/schema.graphql',
+	resolvers,
+	context: { prisma },
 })
 server.start(() => console.log('Server is running on http://localhost:4000'))
