412634476c389f474bafba072b7548ddc85b928c
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-08-11 20:37:39 +0200

    Added basic E2E tests

--- nest-graph-ql/test/items.e2e-spec.ts
+++ nest-graph-ql/test/items.e2e-spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { ItemsModule } from '../src/items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
 
 describe('AppController (e2e)', () => {
   let app;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        ItemsModule,
+        MongooseModule.forRoot('mongodb://localhost/nestgraphqltesting'),
+        GraphQLModule.forRoot({
+          autoSchemaFile: 'schema.gql',
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
