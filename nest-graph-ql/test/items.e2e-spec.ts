86f244aba1b6e9a2ed0ca35b489e3fad928907ae
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-08-12 16:37:39 +0200

    Update item E2E test

--- nest-graph-ql/test/items.e2e-spec.ts
+++ nest-graph-ql/test/items.e2e-spec.ts
@@ -4,6 +4,7 @@ import { ItemsModule } from '../src/items/items.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ItemType } from '../src/items/dto/create-item.dto';
+import { Item } from '../src/items/interfaces/item.interface';
 
 describe('AppController (e2e)', () => {
   let app;
@@ -27,17 +28,32 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  const item: ItemType = {
+  const item: Item = {
     title: 'Great item',
     price: 10,
     description: 'Description of this great item',
   };
 
-  const itemObject = JSON.stringify(item).replace(/\"([^(\")"]+)\":/g, '$1:');
+  const id: string = '';
+
+  const updatedItem: Item = {
+    title: 'Great updated item',
+    price: 20,
+    description: 'Updated description of this great item',
+  };
+
+  const createitemObject = JSON.stringify(item).replace(
+    /\"([^(\")"]+)\":/g,
+    '$1:',
+  );
+  const updateItemObject = JSON.stringify(updatedItem).replace(
+    /\"([^(\")"]+)\":/g,
+    '$1:',
+  );
 
   const createItemQuery = `
   mutation {
-    createItem(input: ${itemObject}) {
+    createItem(input: ${createitemObject}) {
       title
       price
       description
@@ -55,6 +71,8 @@ describe('AppController (e2e)', () => {
       })
       .expect(({ body }) => {
         const data = body.data.createItem;
+        console.log(data.id);
+        this.id = data.id;
         expect(data.title).toBe(item.title);
         expect(data.description).toBe(item.description);
         expect(data.price).toBe(item.price);
@@ -71,6 +89,36 @@ describe('AppController (e2e)', () => {
         query: '{items{title, price, description, id}}',
       })
       .expect(({ body }) => {})
+      .expect(200);
+  });
+
+  it('createItem', () => {
+    const updateItemQuery = `
+    mutation {
+      updateItem(id: "${this.id}", input: ${updateItemObject}) {
+        title
+        price
+        description
+        id
+      }
+    }`;
+
+    console.log(updateItemQuery);
+
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        operationName: null,
+        variables: {},
+        query: updateItemQuery,
+      })
+      .expect(({ body }) => {
+        console.log(body);
+        const data = body.data.updateItem;
+        expect(data.title).toBe(updatedItem.title);
+        expect(data.description).toBe(updatedItem.description);
+        expect(data.price).toBe(updatedItem.price);
+      })
       .expect(200);
   });
 });
