b0c0f8e1f27ae6c524cacbfbcbd61c0a494d9e7f
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-10-17 22:20:42 +0200

    Deleted all view files and dependencies

--- express-auth-boilerplate/backend/app.js
+++ express-auth-boilerplate/backend/app.js
@@ -5,9 +5,7 @@ const app = express()
 app.use(bodyParser.json())
 
 const userRouter = require('./routes/userRouter')
-const appRouter = require('./routes/appRouter')
 
 app.use('/auth', userRouter)
-app.use('/', appRouter)
 
 module.exports = app
--- express-auth-boilerplate/backend/server.js
+++ express-auth-boilerplate/backend/server.js
@@ -1,8 +1,8 @@
 const http = require('http')
 const mongoose = require('mongoose')
 const app = require('./app')
-const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const port = 3000
 
@@ -15,12 +15,9 @@ mongoose
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err))
 
-
+app.use(cors())
 app.use(bodyParser());
 app.set('port', port)
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 const server = http.createServer(app)
 
